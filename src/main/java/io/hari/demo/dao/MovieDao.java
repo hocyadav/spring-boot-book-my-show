c2623bc739e0c23c1d43fa46a2df7a6956775b3a
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-03 18:41:57 +0530

    service

---
+++ src/main/java/io/hari/demo/dao/MovieDao.java
@@ -0,0 +1,12 @@
+package io.hari.demo.dao;
+
+import io.hari.demo.entity.Movie;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author Hariom Yadav
+ * @create 02-03-2021
+ */
+@Repository
+public interface MovieDao extends BaseDao<Movie> {
+}
---
+++ src/main/java/io/hari/demo/dao/ScreenDao.java
@@ -0,0 +1,15 @@
+package io.hari.demo.dao;
+
+import io.hari.demo.entity.Screen;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @Author Hariom Yadav
+ * @create 02-03-2021
+ */
+@Repository
+public interface ScreenDao extends BaseDao<Screen> {
+    List<Screen> findAllByShows_Id(Long showId);
+}
