f8a12d0dbd321bfade85f7b0c421ffe39c2ef135
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-03 18:42:18 +0530

    service

---
+++ src/main/java/io/hari/demo/dao/ShowDao.java
@@ -0,0 +1,12 @@
+package io.hari.demo.dao;
+
+import io.hari.demo.entity.Show;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author Hariom Yadav
+ * @create 02-03-2021
+ */
+@Repository
+public interface ShowDao extends BaseDao<Show> {
+}
---
+++ src/main/java/io/hari/demo/dao/TicketDao.java
@@ -0,0 +1,13 @@
+package io.hari.demo.dao;
+
+import io.hari.demo.entity.Ticket;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author Hariom Yadav
+ * @create 02-03-2021
+ */
+@Repository
+public interface TicketDao extends BaseDao<Ticket> {
+
+}
